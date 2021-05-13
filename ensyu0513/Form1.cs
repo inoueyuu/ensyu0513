3b2ba2a98ef1dc1a824c06bb101ff430a11d1b0f
Yuki <>
2021-05-13 12:06:13 +0900

    Initial commit

---
+++ ensyu0513/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ensyu0513
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            button1.Visible = false;
+            button2.Visible = true;
+            button3.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            button1.Visible = true;
+            button3.Visible = true;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+            button1.Visible = true;
+            button2.Visible = true;
+        }
+    }
+}
